a6fdd3b565859068ad55fa23608ad78c318023d4
Björn Petersen <git@bjoernpetersen.net>
2023-11-18 14:47:56 +0100

    Add telegram-video-reverse-bot

--- config.py
+++ config.py
@@ -176,6 +176,13 @@ ORGANIZATIONS = [
         "token_environment_variable_name": "GITHUB_TOKEN_PREPARINGFOREXAMS",
         "repos": [
             {
+                "name": "telegram-video-reverse-bot",
+                "serviceaccount": {
+                    "name": "github",
+                    "namespace": "telegram-bots",
+                }
+            },
+            {
                 "name": "telegram-vreddit-bot",
                 "serviceaccount": {
                     "name": "github",
